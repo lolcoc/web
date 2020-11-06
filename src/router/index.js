bb55c33c2ac9858c20dd7e234c46f37089e9f164
duan <442492290@qq.com>
2020-11-06 16:34:40 +0800

    添加测试页

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+/**
+ * 第一步   引入文件
+ */
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
+/**
+ * 第二步   添加路由
+ * mode  必填
+ */
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
     }
   ]
 })
