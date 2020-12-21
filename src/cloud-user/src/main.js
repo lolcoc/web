c82424211d4a3a0ff1a2f0e12d234bbf8a3a5849
duan <442492290@qq.com>
2020-12-21 16:57:27 +0800

    登录

--- src/cloud-user/src/main.js
+++ src/cloud-user/src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+/* 引入element UI */
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App'
 import router from './router'
-// 引入axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8002/user/'
-// 修改Vue的原型属性
 Vue.prototype.$ajax = axios
 
 Vue.config.productionTip = false
+/* 引入element UI */
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
--- src/cloud-user/src/router/index.js
+++ src/cloud-user/src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
+/**
+ * 第一步   引入文件
+ */
+import Login from '@/components/user/Login'
 
 Vue.use(Router)
 
+/**
+ * 第二步   添加路由
+ * mode  必填
+ */
 export default new Router({
   routes: [
     {
