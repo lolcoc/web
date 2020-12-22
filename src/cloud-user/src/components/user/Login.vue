d3e68067a953cfb0ae0e8ea336dba3d15e3b020e
duan <442492290@qq.com>
2020-12-22 09:10:08 +0800

    登录的js以及背景

--- src/cloud-user/src/components/user/Login.vue
+++ src/cloud-user/src/components/user/Login.vue
@@ -31,7 +31,7 @@
                 </el-col>
                 <el-col :span="14">
                   <div class="login-center-div-user-input">
-                    <el-input v-model="input" placeholder="请输入内容"></el-input>
+                    <el-input v-model="username" placeholder="请输入内容"></el-input>
                   </div>
                 </el-col>
                 <el-col :span="2">
@@ -47,7 +47,7 @@
                 </el-col>
                 <el-col :span="14">
                   <div class="login-center-div-user-input">
-                    <el-input placeholder="请输入密码" v-model="input" show-password></el-input>
+                    <el-input placeholder="请输入密码" v-model="password" show-password></el-input>
                   </div>
                 </el-col>
                 <el-col :span="2">
@@ -55,21 +55,21 @@
                 </el-col>
               </el-row>
               <el-row>
-                <el-col :span="12">
+                <el-col :span="14">
                   <div class="login-center-div-button"></div>
                 </el-col>
-                <el-col :span="2">
+                <el-col :span="1">
                   <div class="login-center-div-button">
-                    <el-button type="success">注册按钮</el-button>
+                    <el-button type="success">注册</el-button>
                   </div>
                 </el-col>
                 <el-col :span="2">
                   <div class="login-center-div-button">
                   </div>
                 </el-col>
-                <el-col :span="2">
+                <el-col :span="1">
                   <div class="login-center-div-button">
-                    <el-button type="success">成功按钮</el-button>
+                    <el-button type="success" id="login" @click="login()">登录</el-button>
                   </div>
                 </el-col>
               </el-row>
@@ -89,10 +89,16 @@
   </el-row>
 </template>
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
   .all {
     width: 100%;
     height: 100ch;
-    background-color: antiquewhite;
+    background: url("../../assets/background.jpg") no-repeat;
+    background-size: 100%;
   }
 
   .login-split-3-div {
@@ -113,7 +119,6 @@
   .login-center-div-area {
     width: 100%;
     height: 400px;
-    background-color: lightyellow;
     border-radius: 30px;
   }
 
@@ -144,3 +149,29 @@
     height: 100px;
   }
 </style>
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () {
+      console.log(this.username)
+      console.log(this.password)
+      this.$http.post('localhost:8080/user/login', {
+        username: this.username,
+        password: this.password
+      }, {emulateJSON: true}).then(function (res) {
+        console.log(res.data)
+        window.location.href = 'index.html'
+      }, function (res) {
+        console.log(res.status)
+      })
+    }
+  }
+}
+</script>
